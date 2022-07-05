dc61189af2918a48d8607737cd3037e26f3beac9
Sri Varun M <39323584+SriVarun126@users.noreply.github.com>
2022-07-05 23:11:14 +0530

    Create 2. Vectors in more detail.v

---
+++ 2. Verilog Language/2. Vectors/2. Vectors in more detail.v
@@ -0,0 +1,7 @@
+`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
+module top_module( 
+    input wire [15:0] in,
+    output wire [7:0] out_hi,
+    output wire [7:0] out_lo );
+    assign {out_hi,out_lo}={in[15:8],in[7:0]};
+endmodule
