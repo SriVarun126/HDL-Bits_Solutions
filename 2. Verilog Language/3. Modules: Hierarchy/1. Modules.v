4b3a413485278beb9f8b56451306ec61d8c6864f
Sri Varun M <39323584+SriVarun126@users.noreply.github.com>
2022-07-05 23:01:51 +0530

    Create 1. Modules.v

---
+++ 2. Verilog Language/3. Modules: Hierarchy/1. Modules.v
@@ -0,0 +1,10 @@
+//Ports by name
+module top_module ( input a, input b, output out );
+    mod_a inst(.in1(a),.in2(b),.out(out));
+endmodule
+
+
+//Ports by position
+module top_module ( input a, input b, output out );
+    mod_a inst(a,b,out);
+endmodule
