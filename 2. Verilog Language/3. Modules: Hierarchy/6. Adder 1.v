7d506032f975bd3f6528522d5ed436ff27c0fef0
Sri Varun M <39323584+SriVarun126@users.noreply.github.com>
2022-07-05 23:25:51 +0530

    Create 6. Adder 1.v

---
+++ 2. Verilog Language/3. Modules: Hierarchy/6. Adder 1.v
@@ -0,0 +1,9 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire temp1,temp2;
+    add16 inst1(a[15:0],b[15:0],0,sum[15:0],temp1);
+    add16 inst2(a[31:16],b[31:16],temp1,sum[31:16],temp2);
+endmodule
