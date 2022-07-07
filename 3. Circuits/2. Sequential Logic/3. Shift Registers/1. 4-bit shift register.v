7fca173445a46bb294d59d230b972adc2026cd55
Sri Varun M <39323584+SriVarun126@users.noreply.github.com>
2022-07-07 10:14:31 +0530

    Create 1. 4-bit shift register.v

---
+++ 3. Circuits/2. Sequential Logic/3. Shift Registers/1. 4-bit shift register.v
@@ -0,0 +1,16 @@
+module top_module(
+    input clk,
+    input areset,  // async active-high reset to zero
+    input load,
+    input ena,
+    input [3:0] data,
+    output reg [3:0] q); 
+    always @(posedge clk or posedge areset) begin
+        if(areset) q<=4'b0000;
+        else begin
+            if(load) q<=data;
+            else if(ena) q<={1'b0,q[3:1]};
+            else q<=q;
+        end
+    end
+endmodule
