203022663fe840e3578a81dd9f363fe0e1acac3a
Sri Varun M <39323584+SriVarun126@users.noreply.github.com>
2022-07-15 17:49:52 +0530

    Update 20. Sequence recognition.v

--- 3. Circuits/2. Sequential Logic/5. Finite State Machines/20. Sequence recognition.v
+++ 3. Circuits/2. Sequential Logic/5. Finite State Machines/20. Sequence recognition.v
@@ -5,8 +5,8 @@ module top_module(
     output disc,
     output flag,
     output err);
-	parameter A=0,B=1,C=2,D=3,E=4,F=5,G=6,H=7,I=8,J=9;
-    reg [4:0]state,next_state;
+    parameter A=0,B=1,C=2,D=3,E=4,F=5,G=6,H=7,I=8,J=9;
+    reg [3:0]state,next_state;
     always @(*) begin
         case(state)
             A:next_state<=in?B:A;
